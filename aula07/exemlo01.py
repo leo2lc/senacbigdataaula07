0f20c5deb76902ed4cfaabcb539f143ee72566f8
leo2lc <leonardolucas-c@hotmail.com>
2025-12-04 19:29:50 -0300

    Iniciando trabalho com listas

---
+++ aula07/exemlo01.py
@@ -0,0 +1,17 @@
+# Listas
+# São variáveis compostas para armazenamento de mais informações por vez
+# É caraterizada no Python pelo símbolo []
+# Por convenção, usamos o nome das listas no Plural. É opcional o prefixo "list", "lst"
+
+list_meses = ["janeiro","fevereiro","março","abril","maio","junho"]
+
+print(list_meses)
+print(list_meses[0]) # primeiro
+print(list_meses[1]) # segundo
+print(list_meses[2]) # terceiro
+print(list_meses[-1]) # último
+print(list_meses[-2]) # penúltimo...e por ai vai
+print(list_meses[1:4]) # uma determinado intervalo, não vai até o 4
+print(list_meses[2:]) # de um determinado número até o último
+print(list_meses[:5]) # de um determinado número até o último
+print(list_meses[0:6:2]) 
